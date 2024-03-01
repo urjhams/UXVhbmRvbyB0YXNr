28ea4fe0363eb77f966e2ee374d0a507f363c3ea
Quân Đinh <17819030+urjhams@users.noreply.github.com>
2024-03-01 07:35:38 +0100

    refactor swiftUI cell using viewBuilder

--- QuandooTask/View/PostView.swift
+++ QuandooTask/View/PostView.swift
@@ -10,12 +10,7 @@ struct PostView: View {
   var body: some View {
     VStack {
       List(model.posts) { post in
-        VStack(alignment: .leading, spacing: 5) {
-          Text(post.title)
-            .font(.title3)
-            .fontWeight(.bold)
-          Text(post.body)
-        }
+        PostCell(of: post)
       }
     }
     .onAppear {
@@ -23,6 +18,17 @@ struct PostView: View {
         // load the model when appear
         model.posts = (try? await FetchingService.shared.getPosts(userId: userId)) ?? []
       }
+    }
+  }
+}
+
+extension PostView {
+  @ViewBuilder private func PostCell(of post: Post) -> some View {
+    VStack(alignment: .leading, spacing: 5) {
+      Text(post.title)
+        .font(.title3)
+        .fontWeight(.bold)
+      Text(post.body)
     }
   }
 }
